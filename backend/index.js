9e50e782666060ee7eeddf917d151d4962da95cd
Kashish Plahnian <kplahnian@gmail.com>
2025-12-30 18:05:43 +0530

    Add Backend

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+console.log("BACKEND FILE EXECUTED");
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const certificates = {
+  "0": {
+    certificateId: 0,
+    studentName: "Kashish",
+    course: "B.Tech CSE",
+    grade: "A",
+    issueDate: "2025-01-01",
+    isValid: true
+  },
+  "2": {
+    certificateId: 2,
+    studentName: "Rachna",
+    course: "B.Tech CSE",
+    grade: "A",
+    issueDate: "2025-01-01",
+    isValid: false
+  }
+};
+
+
+app.get("/api/certificates/:id", (req, res) => {
+  const cert = certificates[req.params.id];
+  if (!cert) {
+    return res.status(404).json({ error: "Certificate not found" });
+  }
+  res.json(cert);
+});
+
+app.listen(7000, () => {
+  console.log("Backend running on port 7000");
+});
